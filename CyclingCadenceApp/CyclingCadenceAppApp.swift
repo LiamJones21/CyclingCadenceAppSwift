f0e7d41220b3ae00d8c0fa0cee610e9aa1440117
LiamJones21 <92636294+LiamJones21@users.noreply.github.com>
2024-10-20 20:56:59 +0100

    Initial Commit

---
+++ CyclingCadenceApp/CyclingCadenceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclingCadenceAppApp.swift
+//  CyclingCadenceApp
+//
+//  Created by Jones, Liam on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CyclingCadenceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CyclingCadenceAppTests/CyclingCadenceAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CyclingCadenceAppTests.swift
+//  CyclingCadenceAppTests
+//
+//  Created by Jones, Liam on 20/10/2024.
+//
+
+import Testing
+@testable import CyclingCadenceApp
+
+struct CyclingCadenceAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
